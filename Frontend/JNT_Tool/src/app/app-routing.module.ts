cd90dc4615af8cfd3434d5ea5203ce6d47143db3
dev <dev@joenish.com>
2023-11-30 12:46:20 +0530

    tenant base structure

--- Frontend/JNT_Tool/src/app/app-routing.module.ts
+++ Frontend/JNT_Tool/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TenantListComponent } from './tenant-list/tenant-list.component';
+import { SignupComponent } from './sign-up/sign-up.component';
+import { AuthGuard } from './auth/auth.guard';
+import { LoginComponent } from './login/login.component';
+
+
+
+const routes: Routes = [{path:"mainpage",component:TenantListComponent ,canActivate: [AuthGuard]},{path:"signup",component:SignupComponent},{path:'login',component:LoginComponent}];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/JNT_Tool/src/app/app.component.spec.ts
+++ Frontend/JNT_Tool/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'JNT_Tool'`, () => {
+  it(`should have as title 'TenantManagementSystem'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('JNT_Tool');
+    expect(app.title).toEqual('TenantManagementSystem');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('JNT_Tool app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('TenantManagementSystem app is running!');
   });
 });
