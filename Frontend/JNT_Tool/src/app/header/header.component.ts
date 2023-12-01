1bff2f956664df7b8326f38a0c956a900bf850f4
dev <dev@joenish.com>
2023-12-01 17:42:17 +0530

    Header changes

--- Frontend/JNT_Tool/src/app/header/header.component.ts
+++ Frontend/JNT_Tool/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { createClient } from '@supabase/supabase-js';
 import { SupabaseService } from '../supabase.service';
 import { Router } from '@angular/router';
@@ -9,17 +9,15 @@ import Swal from 'sweetalert2';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent{
+export class HeaderComponent implements OnInit {
   supabase = createClient(
     'https://lqviihvmwdkabqlpecxh.supabase.co',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxdmlpaHZtd2RrYWJxbHBlY3hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkzMzgxNDAsImV4cCI6MjAxNDkxNDE0MH0.970stIqUsgdhPxejzbb-6R39pDOAx3J4rIGWz_c6ZAM'
   );
-  constructor(private auth: SupabaseService,private router: Router,)
-  {
 
-  }
-  ngOnInIt()
-  {
+  constructor(private auth: SupabaseService, private router: Router) {}
+
+  ngOnInit() {
     const storedFirstName = localStorage.getItem('tenantName');
 
     // Set the value to loggedInUserName if it exists
@@ -27,10 +25,12 @@ export class HeaderComponent{
       this.loggedInUserName = storedFirstName;
     }
   }
+
   loggedInUserName: string = '';
+
   logOut() {
     this.auth.signOut().then(() => {
-      localStorage.removeItem('token')
+      localStorage.removeItem('token');
       this.router.navigate(['/login']);
     });
     Swal.fire({
