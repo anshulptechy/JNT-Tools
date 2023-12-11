7e5da7cd816de12acd4d6e9852d84d0126e6d716
CouponManagementFrontend <upasanaJoenish@gmail.com>
2023-12-11 15:42:35 +0530

    Date validations

--- Frontend/JNT_Tool/src/app/couponComponents/add/add.component.ts
+++ Frontend/JNT_Tool/src/app/couponComponents/add/add.component.ts
@@ -22,22 +22,21 @@ export class AddComponent {
     private serve: CouponService,
     private _supaService: SupabaseService,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) {
+  )  {
     this.couponForm = this.fb.group({
       // Initializing the form with default values and validation rules
       id: [0],
       couponCode: ['string'],
       couponName: ['', [Validators.required]],
       description: [''],
-      discount: ['', [Validators.required, Validators.min(1)]],
+      discount: ['', [Validators.required, Validators.min(0)]],
       quantity: ['', [Validators.required, Validators.min(1)]],
       startDate: ['', [Validators.required]],
       endDate: ['', [Validators.required]],
       discountType: ['', [Validators.required]],
-      supabaseUserId: ['', [Validators.required]]
-    });
+      supabaseUserId: ['', [Validators.required]],
+    }, { validator: this.dateValidator.bind(this) }); // Add custom validator to the form group
   }
-
 
   async ngOnInit() {
     // Fetch user details when the dashboard component initializes
@@ -54,8 +53,8 @@ export class AddComponent {
     if (this.couponForm.invalid) {
       Swal.fire({
         icon: 'error',
-        title: 'Invalid',
-        text: ' *Please fill in all the required fields!',
+        title: 'Invalid Form',
+        text: ' *Please validate all the required fields!',
       });
       // return;
     }
@@ -91,17 +90,33 @@ export class AddComponent {
   dateValidator(form: FormGroup) {
     const startDateControl = form.get('startDate');
     const endDateControl = form.get('endDate');
-
+  
     if (startDateControl && endDateControl) {
       const startDate = startDateControl.value;
       const endDate = endDateControl.value;
-
-      if (startDate && endDate && new Date(startDate) >= new Date(endDate)) {
-        endDateControl.setErrors({ dateError: true });
+  
+      // Get the current date in the local timezone
+      const currentDate = new Date();
+      currentDate.setHours(0, 0, 0, 0); 
+  
+      // Parse the selected start date and end date strings to Date objects
+      const parsedStartDate = startDate ? new Date(startDate) : null;
+      const parsedEndDate = endDate ? new Date(endDate) : null;
+  
+      // Check if the selected start date is in the past or the same as the current date
+      if (parsedStartDate && parsedStartDate < currentDate) {
+        startDateControl.setErrors({ pastDateError: 'Please choose a date from today or later.' });
+      } else {
+        startDateControl.setErrors(null);
+      }
+  
+      // Check if the end date is before the start date or the same as the start date
+      if (parsedStartDate && parsedEndDate && parsedEndDate < parsedStartDate) {
+        endDateControl.setErrors({ dateError: 'End date must be after the start date.' });
       } else {
         endDateControl.setErrors(null);
       }
     }
   }
-
-}
+  
+  }  
