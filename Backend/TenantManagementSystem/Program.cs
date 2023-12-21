221e395ad279c17592b351fac0d91033d6693863
divyanshuRawat12 <divyanshu@joenish.com>
2023-12-21 16:09:07 +0530

    droplet v2

--- Backend/TenantManagementSystem/Program.cs
+++ Backend/TenantManagementSystem/Program.cs
@@ -20,7 +20,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Sqlconnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 builder.Services.AddControllers();
