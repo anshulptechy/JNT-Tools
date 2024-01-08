56c2ab70eae701bf16879135651c014c19613b68
divyanshu <d.rawat2002@gmail.com>
2024-01-08 11:00:24 +0530

    v110

--- Backend/TenantManagementSystem/Program.cs
+++ Backend/TenantManagementSystem/Program.cs
@@ -56,9 +56,14 @@ builder.Services.AddScoped(typeof(IApplyLeaveRepo<>), typeof(ApplyLeaveRepo<>));
 
 
 
+
 builder.Services.AddScoped(typeof(ISalaryReport<>), typeof(SalaryReport<>));
 builder.Services.AddScoped<ISalaryService, SalaryService>();
 
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.HttpsPort = 443; // Make sure this is set to the correct HTTPS port
+});
 
 
 var app = builder.Build();
@@ -66,6 +71,8 @@ var app = builder.Build();
 
     app.UseSwagger();
     app.UseSwaggerUI();
+
+
 
 
 app.UseHttpsRedirection();
